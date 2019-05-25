d342a0a8f62d852cc86ca34e1792f5149be09d42
MuhammadHasan <hasan.idrees@outlook.com>
2019-05-25 18:35:35 +0500

    learn interpolation

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: 'app-hello', // use selector as html tag <app-hello>
+  selector: 'app-hello', // use selector as html tag <app-hello>
   // selector: '.app-hello', //use selector as the class name .app-hello.
-  selector: '[app-hello]', // we can also use this [] bracket.
-  // templateUrl: './hello.component.html',
-  template: `
-    <div> inline Template</div>
-  `,
-  // styleUrls: ['./hello.component.css']
-  styles: [`
-    div{
-      background-color: aqua;
-      color: red;
-    }
-  `]
+  // selector: '[app-hello]', // we can also use this [] bracket.
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
 })
 export class HelloComponent implements OnInit {
+
+  name: string = 'hasan';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getName(){
+    return this.name;
   }
 
 }
