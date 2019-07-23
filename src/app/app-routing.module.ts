5c1b1b6a03a5e7be55edd98f38066c07051de891
MuhammadHasan <hasan.idrees@outlook.com>
2019-07-23 22:54:37 +0500

    basic routing is done.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesDetailComponent } from './courses-detail/courses-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'course', component: CoursesListComponent},
+  { path: 'course-detail', component: CoursesDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
